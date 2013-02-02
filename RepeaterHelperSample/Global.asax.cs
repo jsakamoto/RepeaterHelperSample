4b5696b464b4ae6865248e37501f694a42602384
Sakamoto <junichi.jp@gmail.com>
2013-02-02 21:51:31 +0900

    1st commit.

---
+++ RepeaterHelperSample/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RepeaterHelperSample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            RouteTable.Routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+    }
+}
---
+++ RepeaterHelperSample/RepeaterHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+using System.Web.WebPages.Html;
+
+namespace RepeaterHelperSample
+{
+    public static class RepeaterHelper
+    {
+        public static HelperResult Repeater<T1>(this HtmlHelper helper, IEnumerable<T1> items, 
+            Func<T1, HelperResult> template, 
+            Func<T1, HelperResult> separator = null,
+            Func<T1, HelperResult> empty = null)
+        {
+            var result = new HelperResult(writer =>
+            {
+                foreach (var itemWithIndex in items.Select((item, index) => new { item, index }))
+                {
+                    if (itemWithIndex.index > 0 && separator != null)
+                    {
+                        separator(itemWithIndex.item).WriteTo(writer);
+                    }
+                    template(itemWithIndex.item).WriteTo(writer);
+                }
+                if (items.Any() == false && empty != null)
+                {
+                    empty(default(T1)).WriteTo(writer);
+                }
+            });
+            return result;
+        }
+    }
+}
