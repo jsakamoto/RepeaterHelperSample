c709cf871dbdec39ee7564787861b93aee29eb1d
Sakamoto <junichi.jp@gmail.com>
2013-02-03 21:57:52 +0900

    Add Repeater Type2 implements.

--- RepeaterHelperSample/RepeaterHelper.cs
+++ RepeaterHelperSample/RepeaterHelper.cs
@@ -30,5 +30,31 @@ namespace RepeaterHelperSample
             });
             return result;
         }
+
+        /// <summary>
+        /// Repeater Helper Type2
+        /// </summary>
+        public static HelperResult Repeater<T1>(this HtmlHelper helper, IEnumerable<T1> items,
+            Func<T1, Func<RepeaterItem<T1>, HelperResult>> template,
+            Func<RepeaterItem<T1>, HelperResult> separator = null,
+            Func<RepeaterItem<T1>, HelperResult> empty = null)
+        {
+            var result = new HelperResult(writer =>
+            {
+                foreach (var itemWithIndex in items.Select((item, index) => new RepeaterItem<T1>(item, index)))
+                {
+                    if (itemWithIndex.Index > 0 && separator != null)
+                    {
+                        separator(itemWithIndex).WriteTo(writer);
+                    }
+                    template(itemWithIndex.Item)(itemWithIndex).WriteTo(writer);
+                }
+                if (items.Any() == false && empty != null)
+                {
+                    empty(default(RepeaterItem<T1>)).WriteTo(writer);
+                }
+            });
+            return result;
+        }
     }
 }
---
+++ RepeaterHelperSample/RepeaterItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepeaterHelperSample
+{
+    public class RepeaterItem<T1>
+    {
+        public int Index { get; protected set; }
+
+        public bool Even { get { return (this.Index % 2) == 0; } }
+
+        public bool Odd { get { return !this.Even; } }
+
+        public T1 Item { get; protected set; }
+
+        public RepeaterItem(T1 item, int index)
+        {
+            this.Item = item;
+            this.Index = index;
+        }
+    }
+}
